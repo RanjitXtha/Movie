92bff09e4f2208634d32061860c9527a9a1a8a95
Ranjit <shrestha.ranjit.np@gmail.com>
2024-10-28 17:05:55 +0545

    api works

---
+++ backend/tmdb/popularMovies.js
@@ -0,0 +1,18 @@
+const access_token_key = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2N2E2ZDViMzEzZDBhMWNmZDlkYTlmOWJkMGU0ZTQ3NSIsIm5iZiI6MTczMDExMjgzMi4wMjg4NTEsInN1YiI6IjY3MWY2YWI1MWVhMzM5MjgyOTdkYjcxZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2awqv6l8i_1r-MPvnOjmV09Di_ocFNol5hGlOJUOeCA';
+const API_KEY = '67a6d5b313d0a1cfd9da9f9bd0e4e475';
+const BASE_URL = 'https://api.themoviedb.org/3';
+
+const fetchPopularMovies = async(req,res) =>{
+    try {
+      const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      console.log(data.results); 
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
+
+module.exports = (fetchPopularMovies};
--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,6 +1,22 @@
 import React from 'react';
 
 const Homepage = () => {
+  const API_KEY = '67a6d5b313d0a1cfd9da9f9bd0e4e475';
+const BASE_URL = 'https://api.themoviedb.org/3';
+  async function fetchPopularMovies() {
+    try {
+      const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      console.log(data.results); // Logs popular movies
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
+  
+  fetchPopularMovies();
   return (
     <div>Homepagee
        <div>new page is added</div>
