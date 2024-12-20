76f1b214311158734b962ee9a14ce1d91517ce45
RanjitXtha <132259585+RanjitXtha@users.noreply.github.com>
2024-12-20 21:02:24 +0545

    changes mde

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -42,6 +42,7 @@ const LoginPage = () => {
     } else {
       const error = await response.json();
       setError(error.message);
+      console.log(error.message)
     }}catch(err){
       console.log(err);
     }
